e0026cd784022ed5dc8c4f9452b6c129fecc6a89
li_jun <395711070@qq.com>
2020-05-06 09:26:25 +0800

    :hammer_and_pick: 调整代码结构

--- utils/word2vec_builder.py
+++ utils/word2vec_builder.py
@@ -5,7 +5,7 @@ from gensim.models import FastText
 from gensim.models.word2vec import LineSentence
 from gensim.models.keyedvectors import KeyedVectors
 
-from utils.tools import timeit
+from utils.tools import timeit, load_lines_from_path
 
 
 def load_data_from_file(path):
@@ -69,12 +69,24 @@ def model_test(model, kw_1, kw_2):
     print('{} similar verbs contains：{}'.format(kw_2, model.wv.similar_by_word(kw_2)))
 
 
+# 根据字典构建embedding_matrix
+@timeit
+def embedding_vector_matrix_build(model, word2idx):
+    matrix = {}
+    for word in model.vocab:
+        if word not in word2idx.keys():
+            continue
+        matrix[word2idx[word]] = model.get_vector(word)
+    return matrix
+
+
 if __name__ == '__main__':
     train_x_cut_file_path = '../resource/gen/train_x_cut.txt'
     train_y_cut_file_path = '../resource/gen/train_y_cut.txt'
     test_x_cut_file_path = '../resource/gen/test_x_cut.txt'
+    vocab_path = '../resource/gen/vocab.txt'
 
-    # gen
+    # gen file path
     all_cut_lines_file_path = '../resource/gen/all_cut_file_path.txt'
 
     word2vec_file_path = '../resource/gen/word2vec.txt'
@@ -83,20 +95,27 @@ if __name__ == '__main__':
     word2vec_ft_file_path = '../resource/gen/word2vec_ft.txt'
     word2vec_ft_bin_file_path = '../resource/gen/word2vec_ft_bin.txt'
 
-    all_cut_lines = []
-    all_cut_lines += load_data_from_file(train_x_cut_file_path)
-    all_cut_lines += load_data_from_file(train_y_cut_file_path)
-    all_cut_lines += load_data_from_file(test_x_cut_file_path)
+    # 训练开关，缺省关闭
+    train_switch = False
 
-    save_data_to_file(all_cut_lines, all_cut_lines_file_path)
+    # 训练词向量
+    if train_switch:
+        all_cut_lines = []
+        all_cut_lines += load_data_from_file(train_x_cut_file_path)
+        all_cut_lines += load_data_from_file(train_y_cut_file_path)
+        all_cut_lines += load_data_from_file(test_x_cut_file_path)
 
-    build_word2vec(all_cut_lines_file_path, word2vec_file_path, word2vec_bin_file_path,
-                   min_count=10, window=5, size=256, sg=1, iter=3)
+        save_data_to_file(all_cut_lines, all_cut_lines_file_path)
 
-    build_word2vec_fast_text(all_cut_lines_file_path, word2vec_ft_file_path, word2vec_ft_bin_file_path, min_count=10,
-                             window=10, size=256, iter=3)
+        build_word2vec(all_cut_lines_file_path, word2vec_file_path, word2vec_bin_file_path,
+                       min_count=10, window=5, size=256, sg=1, iter=3)
 
-    # tests :
+        build_word2vec_fast_text(all_cut_lines_file_path, word2vec_ft_file_path, word2vec_ft_bin_file_path,
+                                 min_count=10,
+                                 window=10, size=256, iter=3)
+
+    # 测试1 : 检查下词向量效果
+    # 加载w2v模型
     w2v_model = KeyedVectors.load_word2vec_format(word2vec_bin_file_path, binary=True)
     model_test(w2v_model, '宝马', '奔驰')
     model_test(w2v_model, '汽车', '减速')
@@ -105,10 +124,40 @@ if __name__ == '__main__':
     model_test(w2v_model, '火花塞', '减震器')
     model_test(w2v_model, '刹车片', '解答')
 
-    w2v_ft_model = KeyedVectors.load_word2vec_format(word2vec_ft_bin_file_path, binary=True)
-    model_test(w2v_ft_model, '宝马', '奔驰')
-    model_test(w2v_ft_model, '汽车', '减速')
-    model_test(w2v_ft_model, '汽车', '车')
-    model_test(w2v_ft_model, '技师', '车主')
-    model_test(w2v_ft_model, '火花塞', '减震器')
-    model_test(w2v_ft_model, '刹车片', '解答')
+    # 加载fast_text模型
+    ft_model = KeyedVectors.load_word2vec_format(word2vec_ft_bin_file_path, binary=True)
+    model_test(ft_model, '宝马', '奔驰')
+    model_test(ft_model, '汽车', '减速')
+    model_test(ft_model, '汽车', '车')
+    model_test(ft_model, '技师', '车主')
+    model_test(ft_model, '火花塞', '减震器')
+    model_test(ft_model, '刹车片', '解答')
+
+    # hw-2 作业：创建embedding_matrix
+    word2idx = {}
+    idx2word = {}
+    vocab_list = load_lines_from_path(vocab_path)
+
+    for line in vocab_list:
+        segments = line.split()
+        word2idx[segments[0]] = segments[1]
+        idx2word[segments[1]] = segments[0]
+
+    # 创建embedding_matrix
+    w2v_embedding_matrix = embedding_vector_matrix_build(w2v_model, word2idx)
+    ft_embedding_matrix = embedding_vector_matrix_build(ft_model, word2idx)
+
+    print('w2v matrix size:{}'.format(len(w2v_embedding_matrix)))
+    print('ft matrix size:{}'.format(len(ft_embedding_matrix)))
+
+    # 测试2：:检查下embedding_matrix有没错
+    t_word = '奔驰'
+    t_idx = word2idx[t_word]
+    print('tWord:{}, tIdx:{}'.format(t_word, t_idx))
+    wv_from_w2v_matrix = w2v_embedding_matrix[t_idx]
+    wv_from_w2v_model = w2v_model.get_vector(t_word)
+    print('wv_from_w2v_matrix == wv_from_w2v_model?:{}'.format((wv_from_w2v_matrix == wv_from_w2v_model)))
+
+    wv_from_ft_matrix = ft_embedding_matrix[t_idx]
+    wv_from_ft_model = ft_model.get_vector(t_word)
+    print('wv_from_ft_matrix == wv_from_ft_matrix?:{}'.format((wv_from_ft_matrix == wv_from_ft_model)))
